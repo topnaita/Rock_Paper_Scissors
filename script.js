7497e3c52423a3c9f89f9eb9eaea90a87bcb8ae1
javier pena <123456789+odin@users.noreply.github.com>
2026-05-16 23:57:03 -0400

    Incomplete third section not returning who wont first round

--- script.js
+++ script.js
@@ -1,28 +1,48 @@
 // This is the the first section of the code where the option is automatically selected by the system.
-function getComputerChoice() {
-  let compInput = Math.floor(Math.random() * 3); // the function math.floor Always Rounds Down
-  if (compInput == 0) {
-    compInput = "Rock";
-  } else if (compInput == 1) {
-    compInput = "Paper";
+function getComputerChoice(compChoice) {
+  if (compChoice == 0) {
+    compChoice = "Rock";
+  } else if (compChoice == 1) {
+    compChoice = "Paper";
   } else {
-    compInput = "Scissor";
+    compChoice = "Scissor";
   }
 
-  return compInput;
+  return compChoice;
 }
-console.log(getComputerChoice());
+
+const pcAuto = (compChoice = Math.floor(Math.random() * 3)); // the function math.floor Always Rounds Down
+console.log(getComputerChoice(pcAuto));
 
 //This is the second section of the game where the user can manually select any option.
 // human select
 
 function getHumanChoice(hChoice) {
-  let strInd = hChoice.charAt(0);
-  let capitalized = strInd.toUpperCase();
-  let restOfString = hChoice.slice(1).toLowerCase();
+  let lowerInput = hChoice.toLowerCase();
 
-  return capitalized + restOfString;
+  if (lowerInput === "rock") {
+    lowerInput = "Rock";
+  } else if (lowerInput === "paper") {
+    hChoice = "Paper";
+  } else if (lowerInput === "scissor") lowerInput = "Scissor";
+
+  return lowerInput;
 }
 
 const message = prompt("Input Your Choice");
 console.log(getHumanChoice(message));
+
+// This is the third section to start comparing one outcome.
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice == "rock" && computerChoice == "paper") {
+    return console.log("You lose! Paper beats Rock");
+  }
+}
+
+const humanSelection = getHumanChoice(message);
+const computerSelection = getComputerChoice(pcAuto);
+playRound(humanSelection, computerSelection);
