c1757abb5d53cf8d2f9950784a1434725b9e785d
javier pena <123456789+odin@users.noreply.github.com>
2026-05-17 22:56:56 -0400

    Unfixed third section still working on outcome

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // This is the the first section of the code where the option is automatically selected by the system.
 function getComputerChoice(compChoice) {
-  if (compChoice == 0) {
+  if (compChoice === 0) {
     compChoice = "Rock";
-  } else if (compChoice == 1) {
+  } else if (compChoice === 1) {
     compChoice = "Paper";
   } else {
     compChoice = "Scissor";
@@ -32,17 +32,20 @@ function getHumanChoice(hChoice) {
 const message = prompt("Input Your Choice");
 console.log(getHumanChoice(message));
 
-// This is the third section to start comparing one outcome.
+// This is the third section to start comparing one outcome of the previous functions.
 
 let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
-  if (humanChoice == "rock" && computerChoice == "paper") {
-    return console.log("You lose! Paper beats Rock");
+  let pOutcome;
+  if (humanChoice === "rock" && computerChoice === 1) {
+    pOutcome = "You lose! Paper beats Rock";
   }
+
+  return pOutcome;
 }
 
-const humanSelection = getHumanChoice(message);
-const computerSelection = getComputerChoice(pcAuto);
-playRound(humanSelection, computerSelection);
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+console.log(playRound(humanSelection, computerSelection));
